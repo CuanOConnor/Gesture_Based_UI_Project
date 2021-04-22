c67afc86f4fc23e49d43f9a5ea32147ec68ba936
Keith Nolan <g00351932@gmit.ie>
2021-04-22 13:44:41 +0100

    Added small script to allow only local players to control their characters

---
+++ ARSpinnerGame/Assets/_Scripts/SetupPlayer.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class SetupPlayer : MonoBehaviourPun
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(photonView.IsMine){
+            //Local player being controlled
+            transform.GetComponent<MovementController>().enabled = true;
+            //Get a handle on local players joystick object
+            transform.GetComponent<MovementController>().joystick.gameObject.SetActive(true); 
+        }
+        else{
+            //Network player disable the movement controller for that player
+             transform.GetComponent<MovementController>().enabled = false;
+            //Disable control of other players character
+            transform.GetComponent<MovementController>().joystick.gameObject.SetActive(false); 
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
